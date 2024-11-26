96d748d62176c49b65a109e19a5ff03774766715
natali.lescano <natali.lescano@valtech.com>
2024-11-26 11:37:31 -0300

    imag2

--- js/main.js
+++ js/main.js
@@ -15,67 +15,8 @@
 
     2. Crear un array de objetos a partir de las frutas de la carpeta img (o de los elementos que prefieran, temática libre)
 
-    3. Imprimir esos objetos por pantalla, deberemos agregar esa funcion a la funcion inicializadora
+    3. Imprimir esos objetos por pantalla, deberemos agregar esa funcion a la funcion inicializadora.
 
-    4. OPCIONAL 1 / Realizar una función filtro que mediante un evento como keyup recoja los datos del input y filtre los productos que contengan esos valores
-
-    5. OPCIONAL 2 / Realizar la funcionalidad de carrito
-
-    6. OPCIONAL 3 / Hacer que esa memoria sea persistente guardando los elementos del carrito en localStorage
-
-
-    Como un mapa mental, escriban con sus palabras el razonamiento con el tienen pensado elaborar cada pregunta
-    Qué tienen pensado hacer en cada uno de estos pasos, qué métodos van a elegir y por qué.
-
-
-    ______________________
-
-    Escriban acá su razonamiento y explicación de todo tu proceso
-*/
-
-
-
-
-/*  PREGUNTA 2_____________
-    Elaboren un array de objetos con las 10 frutas de la carpeta imágenes (o el producto que hayamos colocado ahí)
-    Deben tener como claves: id, nombre, precio y la ruta de la imagen
-*/
-
-////Variables///////////
-
-let cuadriculaProductos = document.querySelector(".product-grid");
-let barraBusqueda = document.querySelector(".search-bar");
-
-let botonesCarrito = document.querySelectorAll(".add-to-cart");
-let objetosCarrito = document.getElementById("cart-items");
-let precioCarrito = document.getElementById("total-prize");
-let contadorCarrito = document.getElementById("cart-counts");
-
-let carrito = [];
-
-////Array de productos/////////////
-
-let productosFruteria = [
-    {id: 1, nombre: "arandano", precio: 5000, img: "/img/arandano.jpg"},
-    {id: 2, nombre: "banana", precio: 1000, img: "/img/banana.jpg"},
-    {id: 3, nombre: "frambuesa", precio: 4000, img: "/img/frambuesa.png"},
-    {id: 4, nombre: "frutilla", precio: 3000, img: "/img/frutilla.jpg"},
-    {id: 5, nombre: "kiwi", precio: 2000, img: "/img/kiwi.jpg"},
-    {id: 6, nombre: "mandarina", precio: 800, img: "/img/mandarina.jpg"},
-    {id: 7, nombre: "manzana", precio: 1500, img: "/img/manzana.jpg"},
-    {id: 8, nombre: "naranja", precio: 9000, img: "/img/naranja.jpg"},
-    {id: 9, nombre: "pera", precio: 3000, img: "/img/pera.jpg"},
-    {id: 10, nombre: "anana", precio: 5000, img: "/img/anana.jpg"},
-    {id: 11, nombre: "pomelo-amarillo", precio: 2000, img: "/img/pomelo-amarillo.jpg"},
-    {id: 12, nombre: "pomelo-rojo", precio: 3000, img: "/img/pomelo-amarillo.jpg"},
-];
-
-
-/*  PREGUNTA 3_____________
-    Agreguen a la funcion inicializadora init() una función para imprimir nombre del grupo en el nav y en la consola.
-*/
-
-    ///Completado al final///
 
 /*  PREGUNTA 4_____________
     Creen una función para imprimir en pantalla los productos del array de objetos y agreguenla a la funcion inicializadora
@@ -89,8 +30,83 @@ let productosFruteria = [
         </div>
 */
 
+    /*  OPCIONAL / PREGUNTA 5_____________
+    Escriban una función filtro, por ejemplo, asociada a un evento keyup, que recoja los valores del campo input y ejecute con cada evento un filtro que actualice los productos
+*/
+
+     /*  OPCIONAL / PREGUNTAS 6 y 7_____________
+
+    1. Elaboren la funcionalidad de carrito. Agreguen funcionalidad al boton de cada producto para introducir ese elemento en un contenedor de carrito e imprimirlo en el listado con id "cart-items"" del HTML
+
+    El HTML que deben agregar debe seguir el siguiente esquema (para que se apliquen los estilos)
+
+    <li class="item-block">
+        <p class="item-name">nombreproducto - $precioproducto</p>
+        <button class="delete-button">Eliminar</button>
+    </li>
+
+    2. Se valorará que se almacenen los productos del carrito en un localStorage
+*/
+
+    ///Como un mapa mental, escriban con sus palabras el razonamiento con el tienen pensado elaborar cada pregunta///
+   ///Qué tienen pensado hacer en cada uno de estos pasos, qué métodos van a elegir y por qué///
+
+
+    //Escriban acá su razonamiento y explicación de todo tu proceso//
+
+
+////  RESOLUCIÓN   //////
+
+
+////Variables///////////
+
+///CREAMOS NUESTRAS VARIABLES LOCALES, QUE APARECEN EN EL INDEX.HTML PARA PODER TRABAJAR CON ELLAS PERO DESDE EL JAVASCRIPT////
+
+let cuadriculaProductos = document.querySelector(".product-grid");
+let barraBusqueda = document.querySelector(".search-bar");
+
+let botonesCarrito = document.querySelectorAll(".add-to-cart");
+let objetosCarrito = document.getElementById("cart-items");
+let precioCarrito = document.getElementById("total-prize");
+let contadorCarrito = document.getElementById("cart-counts");
+
+let carrito = [];
+
+
+
+///Ejercicio 2: Aquí se creo el array de los productos con los que trabajaremos. Almacenando datos como id, nombre, precio e imagen.
+
+////Array de productos/////////////
+
+let productosFruteria = [
+    {id: 1, nombre: "arandano", precio: 5000, img: "img/arandano.jpg"},
+    {id: 2, nombre: "banana", precio: 1000, img: "img/banana.jpg"},
+    {id: 3, nombre: "frambuesa", precio: 4000, img: "img/frambuesa.png"},
+    {id: 4, nombre: "frutilla", precio: 3000, img: "img/frutilla.jpg"},
+    {id: 5, nombre: "kiwi", precio: 2000, img: "img/kiwi.jpg"},
+    {id: 6, nombre: "mandarina", precio: 800, img: "img/mandarina.jpg"},
+    {id: 7, nombre: "manzana", precio: 1500, img: "img/manzana.jpg"},
+    {id: 8, nombre: "naranja", precio: 9000, img: "img/naranja.jpg"},
+    {id: 9, nombre: "pera", precio: 3000, img: "img/pera.jpg"},
+    {id: 10, nombre: "anana", precio: 5000, img: "img/anana.jpg"},
+    {id: 11, nombre: "pomelo-amarillo", precio: 2000, img: "img/pomelo-amarillo.jpg"},
+    {id: 12, nombre: "pomelo-rojo", precio: 3000, img: "img/pomelo-amarillo.jpg"},
+    {id: 13, nombre: "sandia", precio: 5000, img: "img/sandia.jpg"},
+];
+
+
+/*  PREGUNTA 3_____________
+    Agreguen a la funcion inicializadora init() una función para imprimir nombre del grupo en el nav y en la consola.
+*/
+
+    ///AGREGAMOS DICHA FUNCIONALIDAD AL FINALIZAR TODAS LAS DEMAS FUNCIONES Y LINEAS DE CÓDIGOS.///
+
+
+//// PREGUNTA 4////
+
 //////Mostrar productos/////////////// 
-// Función para mostrar productos en la página
+
+// FUNCIÓN PARA MOSTRAR LOS PRODUCTOS EN LA PÁGINA, UTILIZANDO EL ESQUEMA QUE APLICA A LOS ESTILOS DEL HTML////
 
 function mostrarProductos(array) {
 
@@ -110,13 +126,9 @@ function mostrarProductos(array) {
     //console.log(cartaProducto)
 }
 
+/////PUNTO 6 Y 7 REALIZAMOS LA FUNCIONALIDAD CARRITO.
 
-/*  OPCIONAL / PREGUNTA 5_____________
-    Escriban una función filtro, por ejemplo, asociada a un evento keyup, que recoja los valores del campo input y ejecute con cada evento un filtro que actualice los productos
-*/
-
-
-// Función para agregar un producto al carrito
+// Función para agregar un producto al carrito.
 
 function agregarCarrito(id) {
     console.log(`id del producto: ${id}`)
@@ -132,22 +144,8 @@ function agregarCarrito(id) {
     mostrarCarrito();
 }
 
-
-/*  OPCIONAL / PREGUNTAS 6 y 7_____________
-
-    1. Elaboren la funcionalidad de carrito. Agreguen funcionalidad al boton de cada producto para introducir ese elemento en un contenedor de carrito e imprimirlo en el listado con id "cart-items"" del HTML
-
-    El HTML que deben agregar debe seguir el siguiente esquema (para que se apliquen los estilos)
-
-    <li class="item-block">
-        <p class="item-name">nombreproducto - $precioproducto</p>
-        <button class="delete-button">Eliminar</button>
-    </li>
-
-    2. Se valorará que se almacenen los productos del carrito en un localStorage
-*/
-
-///Mostrar carrito
+///Mostrar carrito//// 
+///HACEMOS VISIBLE EL CARRITO EN LA PÁGINA////
 
 function mostrarCarrito() {
     let carritoCompra = "";
@@ -165,10 +163,7 @@ function mostrarCarrito() {
     actualizarContadorYPrecio();
 }
 
-//Se agregaron nuevas funciones
-
-//El método splice() cambia el contenido de un array eliminando elementos existentes 
-//y/o agregando nuevos elementos.
+//Se agregaron nuevas funciones PEDIDAS EN EL EJERCICIO como ser la de eliminar productos y otra para que al mismo tiempo se actualice el contador.
 
 // Función para eliminar un producto del carrito
 
@@ -191,8 +186,8 @@ function actualizarContadorYPrecio() {
     precioCarrito.textContent = `${total}`;
 }
 
-
-// Funcion inicializadora
+///PUNTO 3 ///
+// Función inicializadora
 
 function init() {
     // Acá irian las funciones de arranque de la aplicacion. No se olviden de invocar esta app
@@ -206,6 +201,7 @@ function init() {
     mostrarProductos(productosFruteria);
 
     actualizarContadorYPrecio();
+
 }
 
 init();
